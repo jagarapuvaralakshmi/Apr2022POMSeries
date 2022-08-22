323c3506039ddf49f971bb6cd334a8e26da8df8e
jagarapuvaralakshmi <jagarapu.varalaxmi@gmail.com>
2022-08-22 16:44:47 +0530

    second commit

---
+++ src/main/java/com/qa/opencart/constants/DummyPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.constants;
+
+public class DummyPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hai this is vara");
+
+	}
+
+}
